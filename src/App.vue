320d80a5e0c727a205ef0d9a346b715ddb0ed93d
krzysztoffila <umowy.fila@gmail.com>
2023-11-09 13:35:54 +0100

    Add nav, landing page, footer

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,22 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <NavComponent />
+    <main>
+      <router-view />
+    </main>
+    <Footer />
   </div>
 </template>
+<script>
+import NavComponent from '@/components/nav-component.vue'
+import Footer from '@/components/footer-component.vue'
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+export default {
+  components: {
+    NavComponent,
+    Footer,
+  },
 }
+</script>
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="scss"></style>
