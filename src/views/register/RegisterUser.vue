97d5743aa09781726db28c37f0d514d8f17a4b92
krzysztoffila <umowy.fila@gmail.com>
2023-11-20 12:49:25 +0100

    Cleaning code in RegisterUser

--- src/views/register/RegisterUser.vue
+++ src/views/register/RegisterUser.vue
@@ -138,70 +138,7 @@
             id="profile"
             role="tabpanel"
             aria-labelledby="profile-tab"
-          >
-            <h3 class="register-heading">Apply as a Hirer</h3>
-            <div class="row register-form">
-              <div class="col-md-6">
-                <div class="form-group">
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="First Name *"
-                    value=""
-                  />
-                </div>
-                <div class="form-group">
-                  <input
-                    type="text"
-                    class="form-control"
-                    placeholder="Last Name *"
-                    value=""
-                  />
-                </div>
-                <div class="form-group">
-                  <input
-                    type="email"
-                    class="form-control"
-                    placeholder="Email *"
-                    value=""
-                  />
-                </div>
-                <div class="form-group">
-                  <input
-                    type="text"
-                    maxlength="9"
-                    minlength="9"
-                    class="form-control"
-                    placeholder="Phone *"
-                    value=""
-                  />
-                </div>
-              </div>
-              <div class="col-md-6">
-                <div class="form-group">
-                  <input
-                    type="password"
-                    class="form-control"
-                    placeholder="Password *"
-                    value=""
-                  />
-                </div>
-                <div class="form-group">
-                  <input
-                    type="password"
-                    class="form-control"
-                    placeholder="Confirm Password *"
-                    value=""
-                  />
-                </div>
-                <input
-                  type="submit"
-                  class="btnRegister"
-                  value="Rejestruję się"
-                />
-              </div>
-            </div>
-          </div>
+          ></div>
         </div>
       </form>
     </div>
