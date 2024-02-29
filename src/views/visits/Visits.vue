4fd2a50a0251cb7ab9e55c42aab585417da07ea8
krzysztoffila <umowy.fila@gmail.com>
2024-02-29 19:51:59 +0100

    Testowanie widoku wizyt

--- src/views/visits/Visits.vue
+++ src/views/visits/Visits.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>twoje wizyty</div>
+  <div>Twoje Wizyty</div>
+  <!-- Po lewej umow wizyte.. po prawej kalendarz zeby umowic a pod nim przycisk zeby przejsc dalej -->
 </template>
 
 <script>
